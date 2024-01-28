ca6cef69ca3e1f20f625752b1496babcebfc57d9
Divyanshu <kumardivyansh124@gmail.com>
2024-01-28 23:39:51 +0530

    28jan

--- backend/index.js
+++ backend/index.js
@@ -79,7 +79,16 @@ const Product = mongoose.model("Product",{
 })
 
 app.post('/addproduct',async (req,res)=>{
+    let products = await Product.find({});
+    let id;
+    if(products.length>0)
+    {
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id = last_product.id+1;
+    }
     const product = new  Product({
+        id:id,
         id:req.body.id,
         name:req.body.name,
         image:req.body.image,
