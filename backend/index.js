c656f5bf006717073eabdbfdabb18810e6306626
Divyanshu <kumardivyansh124@gmail.com>
2024-03-05 23:42:36 +0530

    5march

--- backend/index.js
+++ backend/index.js
@@ -207,6 +207,14 @@ app.get('/newcollection',async (req,res)=>{
     res.send(newcollection);
 })
 
+//Creating endpoint for popular in women section
+app.get('/popularinwomen',async (req,res)=>{
+    let products = await Product.find({category:"women"});
+    let popular_in_women = products.slice(0,4);
+    console.log("Popular in women fetched");
+    res.send(popular_in_women);
+})
+
 app.listen(port,(error)=>{
     if (!error) {
         console.log("Server Running on Port "+port)
