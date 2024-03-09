900ba64c110e1d84b39e6a3040049224d6c58b17
Divyanshu <kumardivyansh124@gmail.com>
2024-03-10 01:36:57 +0530

    10march

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -24,7 +24,17 @@ const ShopContextProvider = (props) => {
  
     const addToCart = (itemId) =>{
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}));
-        console.log(cartItems);
+        if(localStorage.getItem('auth-token')){
+          fetch('http://localhost:4000/addtocart',{
+            method:'POST',
+            headers:{
+              Accept:'application/form-data',
+              'auth-token':`${localStorage.getItem('auth-token')}`,
+              'Content-Type':'application/json',
+            },
+            body:JSON({"item":itemId})
+          })
+        }
     }
 
     const removeFromCart = (itemId) =>{
